736332cc50bcf1af2d67da81dc5e4ae390c911fb
joaoPFerC <rm94825@estudante.fieb.edu.br>
2025-09-17 08:05:32 -0300

    component

---
+++ src/assets/pages/produtos/index.jsx
@@ -0,0 +1,15 @@
+const Produtos = () => {
+ 
+    return (
+        <div>
+            <h3>Listagem de Produtos</h3>
+            <ul>
+                <li>Pizza de Calabresa</li>
+                <li>Pizza de Muçarela</li>
+                <li>Pizza Dois Queijos</li>
+            </ul>
+        </div>
+    )
+}
+ 
+export default Produtos;
